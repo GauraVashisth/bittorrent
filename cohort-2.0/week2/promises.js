88ff50d7adf732d12c0d85be59a47e55a9bc3cbb
GauraVashisth <gauravvashisth365@gmail.com>
2024-11-08 23:53:08 +0530

    promises

---
+++ cohort-2.0/week2/promises.js
@@ -0,0 +1,6 @@
+//A promise in JavaScript is an object that represents the eventual completion or failure of an asynchronous operation.
+//It is used for handling asynchronous operations, such as making API calls or reading files, in a more organized and readable way.
+
+function he(params) {
+  
+}
