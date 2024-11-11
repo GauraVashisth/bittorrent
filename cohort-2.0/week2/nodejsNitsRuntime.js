613aecdf4819b7b9131283967a8b3bf1d0c78dd7
GauraVashisth <gauravvashisth365@gmail.com>
2024-11-11 22:45:44 +0530

    express

---
+++ cohort-2.0/week2/nodejsNitsRuntime.js
@@ -0,0 +1,13 @@
+//Topic: Node.js and its runtime
+/*some jargons:
+what is ECMA script?
+  ECMAScript is a scripting language specification on which javascript is based.
+  https://tc39.es/ecma262/#sec-numbers-and-data
+    above website clearly tells the javascript standard
+
+what is javascript?
+  
+what is node.js?
+
+what is bun?
+*/
